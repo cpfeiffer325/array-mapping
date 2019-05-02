6c1c09e5c62ce83c46bffcf5cadd2f066c48fbce
Christian <c_pfeiffer325@yahoo.ca>
2019-05-02 21:07:23 +0000

    Completed mapping exercise

--- array-mapping.js
+++ array-mapping.js
@@ -3,7 +3,11 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+
+var result = input.map(function(arr) {
+  console.log(arr);
+  return Math.sqrt(arr.x * arr.x + arr.y * arr.y);
+});
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
